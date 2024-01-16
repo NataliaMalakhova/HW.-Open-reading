b4acda392ed3137927903bd24cb85bf1d968e482
Natalia Malakhova <89288483488@mail.ru>
2024-01-16 22:34:07 +0300

    Initial commit

---
+++ ДЗ открытие файла.py
@@ -0,0 +1,20 @@
+def my_cook_book():
+    with open('recipes.txt', encoding='utf-8') as file:
+        cook_book = {}
+        for txt in file.read().split('\n\n'):
+            name, _, *args = txt.split('\n')
+            tmp = []
+            for arg in args:
+                ingredient_name, quantity, measure = map(lambda x: int(x) if x.isdigit() else x, arg.split(' | '))
+                tmp.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
+            cook_book[name] = tmp
+    return cook_book
+
+def get_shop_list_by_dishes(dishes, person_count):
+    person_count = int(input('Введите количество человек: '))
+    dishes = input('Введите блюда в расчете на одного человека (через запятую): ') \
+    .lower().split(', ')
+ 
+    shop_list = get_shop_list_by_dishes(dishes, person_count)
+    return(shop_list)
+
